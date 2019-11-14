c2b4d029f15f6a9c40d06519e7997b3b5620dd97
Charlie Hsieh <charlieofficial@gmail.com>
2019-11-14 09:10:39 +0800

    add loggedon info into tmux status-left bar

--- scripts/connected_clients.sh
+++ scripts/connected_clients.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+# Logged On (pty , tty) who - show who is logged on (tty/pts) (terminal / ssh)
+logged_on_session() {
+  local logged_on_session="$(who | wc -l)"
+  local logged_on_msg="(LoggedOn: ${logged_on_session})"
+
+  [[ "${logged_on_session}" -ne 1 ]] && echo -n " ${logged_on_msg} "
+}
+
+
+connected_clients() {
+  local connected_clients="$(tmux list-clients -F '#{line}' | wc -l)"
+
+  [[ $connected_clients -ne 1 ]] && echo -n " Clients ▶▶▶ ${connected_clients} "
+}
 
 main() {
-  local connected_clients="$(tmux list-clients -F '#{line}' | wc -l)"
-  if [[ $connected_clients -ne 1 ]]; then
-    echo -n " Clients ▶▶▶ ${connected_clients} "
-  fi
+  logged_on_session
+  connected_clients
 }
 main
